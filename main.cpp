d71672516cbb65f468fcd0b09b00719539aa0d75
Jörg Wunderlich-Pfeiffer <jwupf@gmx.net>
2024-09-15 13:05:19 +0000

    works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "mega65.h"
 #include <stdio.h>
 #include <conio.h>
+#include "hal.h"
+#include "sid.h"
 
 
 void myputs(const char* str){
@@ -46,33 +48,116 @@ int main(void) {
   conioinit();
   clrscr();
 
-  gotoxy(0,24);
-  myputs("F1 XXXXXXX");  
-  myputs("F3 XXXXXXX");
-  myputs("F5 XXXXXXX");
-  myputs("F7 XXXXXXX");
-  myputs("F2 XXXXXXX");
-  myputs("F4 XXXXXXX");
-  myputs("F6 XXXXXXX");
-  myputs("F8 XXXXXXX");
-  for(unsigned char i = 0; i<80;i+=10){
-    cellcolor(i,24,COLOUR_RED);
-    cellcolor(i+1,24,COLOUR_RED);
-  }
+  // gotoxy(0,24);
+  // myputs("F1 XXXXXXX");  
+  // myputs("F3 XXXXXXX");
+  // myputs("F5 XXXXXXX");
+  // myputs("F7 XXXXXXX");
+  // myputs("F2 XXXXXXX");
+  // myputs("F4 XXXXXXX");
+  // myputs("F6 XXXXXXX");
+  // myputs("F8 XXXXXXX");
+  // for(unsigned char i = 0; i<80;i+=10){
+  //   cellcolor(i,24,COLOUR_RED);
+  //   cellcolor(i+1,24,COLOUR_RED);
+  // }
 
-  while(true){
-    switch(Key k; k.Get()){
-      case 'a':
-      gotoxy(0,0);
-      myputs("AAAAAA\n");
-      break;
-      case '\t':
-      gotoxy(0,0);
-      myputs("BBBBBB\n");
-      break;
-      default:
-      break;
-    }
-  };
+  // voice v;
+  // v.freq_lo = 0x01;
+  // v.freq_hi = 0x02;
+  // v.pw_lo = 0x03;
+  // v.pw_hi = 0x04;
+  // v.ctrl = 0x05;
+  // v.attack = 0x06;
+  // v.decay = 0x07;
+  // v.sustain = 0x08;
+  // v.release = 0x09;
+  
+  // for(unsigned char i = 0; i<7;i++){
+  //   gotoxy(0,i);
+  //   cputhex(v.memory[i],2);
+  // }
+  //gotoxy(0,0);cputhex(v.memory[0],2);
+
+  // volatile Sid sid = *(Sid*)0xd400;
+  // for(unsigned char i = 0; i<0x1d;i++){
+  //    sid.memory[i] = 0;
+  // }
+  // // sid->voice1.pw_lo = 0x0;
+  // // sid->voice1.pw_hi = 0x4;
+  // sid.voice1.freq_hi = 42;
+  // sid.voice1.attack = 0xf;
+  // sid.voice1.decay = 0x5;
+  // sid.voice1.sustain = 0xf;
+  // sid.voice1.release = 0x2;
+
+  // sid.voice1.sawtooth = 1;
+  // sid.voice1.gate = 1;
+  // sid.filter_mode_volume = 0xf;
+
+  // while(true){
+    
+
+  
+
+  //   switch(Key k; k.Get()){
+  //     case 'a':
+  //     gotoxy(0,0);
+  //     myputs("AAAAAA\n");
+  //     break;
+  //     case '\t':
+  //     gotoxy(0,0);
+  //     myputs("BBBBBB\n");
+  //     break;
+  //     default:
+  //     break;
+  //   }
+  // };
+
+
+
+  // uint8_t* S = (uint8_t*)54272; //0xd400
+  // uint8_t W = 17; //: ON INT(RND(TI)*4)+1 GOTO 12,13,14,15
+  // // W = 33: GOTO 15
+  // // W = 65: GOTO 15
+  // // W = 129
+  
+  // S[ 0] = 0;
+  // S[ 1] = 0x1f;
+  // S[ 5] = 97;
+  // S[ 6] = 200;
+  // S[ 4] = 17;
+  // S[24] = 15;
+
+
+  Sid* sid = (Sid*)0xd400;
+  sid->voice1.freq_lo=0;
+  sid->voice1.freq_hi=0x1f;
+  sid->voice1.attack = 6;
+  sid->voice1.decay = 1;
+  sid->voice1.sustain=0xC;
+  sid->voice1.release=0x8;
+  //sid->voice1.gate = 1; // 0x11 -> 17
+  //sid->voice1.triangle = 1;
+  sid->voice1.ctrl_bits = voice_ctrl{
+    .noise = 0,
+    .pulse=0,
+    .sawtooth = 0,
+    .triangle = 1,
+    .disable_osc=0,
+    .ring_mod_osc=0,
+    .sync_osc=0,
+    .gate = 1};
+  sid->voice1.ctrl = 0x11;
+  //sid->memory[ 4] = 17;
+  sid->filter_mode_volume = 0x0f;
+
+  // for(unsigned char i = 0; i<25;i++){
+  //   gotoxy(0,i);
+  //   cputhex(sid.memory[i],2);
+  //   gotoxy(10,i);
+  //   cputdec(sid.memory[i],0,0);
+  // }
+
   return 0;
 }
---
+++ sid.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#pragma pack(push, 1)
+
+typedef unsigned char uint8_t;
+// typedef unsigned short uint16_t;
+
+struct double_register
+{
+    uint8_t lower;
+    uint8_t higher;
+};
+
+union pulse_wave_form
+{
+    uint16_t raw; // bytes are in reverse order!
+    struct access
+    {
+        uint8_t lower : 8;
+        uint8_t higher : 4;
+    };
+};
+
+union voice_ctrl
+{
+    uint8_t raw;
+    struct{
+    uint8_t noise : 1;
+    uint8_t pulse : 1;
+    uint8_t sawtooth : 1;
+    uint8_t triangle : 1;
+    uint8_t disable_osc : 1;
+    uint8_t ring_mod_osc : 1;
+    uint8_t sync_osc : 1;                
+    uint8_t gate : 1;
+    };
+};
+
+union voice
+{
+    uint8_t memory[7];
+    struct
+    {
+        uint8_t freq_lo;
+        uint8_t freq_hi;
+        uint8_t pw_lo;
+        uint8_t : 4;
+        uint8_t pw_hi : 4;
+        union
+        {
+            uint8_t ctrl : 8;
+            voice_ctrl ctrl_bits;
+            struct
+            {
+                uint8_t noise : 1;
+                uint8_t pulse : 1;
+                uint8_t sawtooth : 1;
+                uint8_t triangle : 1;
+                uint8_t disable_osc : 1;
+                uint8_t ring_mod_osc : 1;
+                uint8_t sync_osc : 1;                
+                uint8_t gate : 1;
+            };
+        };
+        uint8_t decay : 4;
+        uint8_t attack : 4;
+        uint8_t release : 4;
+        uint8_t sustain : 4;
+    };
+};
+
+// D400
+union Sid
+{
+    uint8_t memory[0x1d];
+    struct
+    {
+        union
+        {
+            struct
+            {
+                voice voice1;
+                voice voice2;
+                voice voice3;
+            };
+            struct
+            {
+                uint8_t voice1_freq_lo;
+                uint8_t voice1_freq_hi;
+                uint8_t voice1_pw_lo;
+                uint8_t voice1_pw_hi;
+                uint8_t voice1_ctrl;
+                uint8_t voice1_att_dec; // attack, decay
+                uint8_t voice1_sus_rel; // sustain, release
+
+                uint8_t voice2_freq_lo;
+                uint8_t voice2_freq_hi;
+                uint8_t voice2_pw_lo;
+                uint8_t voice2_pw_hi;
+                uint8_t voice2_ctrl;
+                uint8_t voice2_att_dur;
+                uint8_t voice2_sus_rel;
+
+                uint8_t voice3_freq_lo;
+                uint8_t voice3_freq_hi;
+                uint8_t voice3_pw_lo;
+                uint8_t voice3_pw_hi;
+                uint8_t voice3_ctrl;
+                uint8_t voice3_att_dec;
+                uint8_t voice3_sus_rel;
+            };
+        };
+
+        uint8_t filter_freq_lo;
+        uint8_t filter_freq_hi;
+        uint8_t filter_reso_ctrl;
+        uint8_t filter_mode_volume;
+        uint8_t paddle_1;
+        uint8_t paddle_2;
+        uint8_t osc_RNG3;
+        uint8_t envelop_gen3_out;
+    };
+};
+
+#pragma pack(pop)
